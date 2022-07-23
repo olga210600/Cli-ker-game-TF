30dc8bcc1d10b6eca58620a66ce352036dfb432a
olga210600 <89257451+olga210600@users.noreply.github.com>
2022-07-24 00:53:53 +0300

    added new function (exit, music)

--- my game/index.js
+++ my game/index.js
@@ -8,23 +8,27 @@ const exit = document.querySelector('.exit')
 const bossWrapper = document.querySelector('.bossWrapper')
 const fighterBoss = document.querySelector('.fighterBoss')
 const audioTag1 = document.querySelector('.audioTag1')
+const audioTag2 = document.querySelector('.audioTag2')
+const audioTag3 = document.querySelector('.audioTag3')
+const resultAction = document.querySelector('.resultAction')
+const myFighterResult = document.querySelector('.myFighterResult')
 
 let values
 
 let currentBoss
 
+const myFighterInfo = {
+    life: 40
+}
+
 const bosses = [
     {
-        id: 1,
         name: 'Stick Man',
         life: 5,
-        inGame: true,
         image: "img/fighter0.svg",
     }, {
-        id: 2,
         name: 'Nindja Man',
         life: 10,
-        inGame: true,
         image: "img/21039920061621418911.svg",
     },
 ]
@@ -35,8 +39,25 @@ let currentBossIndex = 0;
 const setUserName = (element) => {
 
     localStorage.setItem('player', JSON.stringify(element))
-    let userInfo = localStorage.getItem('player')
 }
+
+const setBossesInfo = () => {
+    localStorage.setItem('bosses', JSON.stringify(bosses))
+}
+
+const setMyFighterInfo = () => {
+    localStorage.setItem('myFighter', JSON.stringify(myFighterInfo))
+}
+
+const getMyFighterInfo = () => {
+
+    let fighter = localStorage.getItem('myFighter')
+    let currentMyFighterInfo = JSON.parse(fighter)
+    console.log(currentMyFighterInfo, 'currentMyFighterInfo')
+    // myFighterResult.innerHTML = currentMyFighterInfo.life
+
+}
+getMyFighterInfo()
 
 
 const validateEmail = (email) => {
@@ -49,8 +70,16 @@ const validateEmail = (email) => {
     return true
 };
 
-function play_single_sound() {
+function play_single_boss_sound() {
    audioTag1.play();
+}
+
+function play_single_repulsed_attack_sound() {
+   audioTag2.play();
+}
+
+function play_single_myFighter_sound() {
+   audioTag3.play();
 }
 
 function getFormElement(event) {
@@ -82,7 +111,8 @@ function getFormElement(event) {
                 }
             }
         }
-        // getUserName()
+        setBossesInfo()
+        setMyFighterInfo()
     }
 }
 
@@ -90,22 +120,6 @@ submit.addEventListener('click', getFormElement)
 
 //first page
 
-let currentSrcFighter = "img/myFighter-second-position.svg"
-
-// const createMyFighter = () => {
-//     return `
-//
-//     <img src= ${currentSrcFighter}>
-//     `
-// }
-
-// createMyFighter()
-
-
-
-//
-// <input type="range" id="cowbell" name="cowbell"
-// min="0" max="${boss.life}" value="${boss.life}" step="1">
 
 
 const createBoss = (boss) => {
@@ -114,17 +128,61 @@ const createBoss = (boss) => {
      
         <h3>${boss.name}</h3>
         <div>
-        
-            <img class="fighterBoss" onclick="bossAttack()"  src=${boss.image}>
+            <img class="fighterBoss" onclick="getResultAttack( getBossAction(3))"  src=${boss.image}>
         </div>
     `
 }
 
-myFighter.img = undefined;
+const myFighterLife = () => {
+    return `
+    <p>${myFighterInfo.life}</p>
+    `
+}
 
+myFighterResult.innerHTML = myFighterLife()
+
+const getBossAction = (max) => {
+   return  Math.floor(Math.random() * max);
+}
+
+const getResultAttack = (value) => {
+    switch (value) {
+        case 0 :
+            resultAction.innerHTML = 'Аттака отбита';
+            secondMusic()
+            break;
+        case 1 :
+            resultAction.innerHTML = ''
+            resultAction.innerHTML = 'уменьшить у босса';
+            bossAttack()
+            break;
+        case 2 :
+            resultAction.innerHTML = ''
+            resultAction.innerHTML = 'уменьшить у моего бойца';
+            thirdMusic()
+            getMyFighterWound()
+            break;
+    }
+}
+
+const getMyFighterWound = () => {
+  console.log(--myFighterInfo.life)
+    setMyFighterInfo()
+    // getMyFighterInfo()
+
+}
+
+const secondMusic = () => {
+   play_single_repulsed_attack_sound()
+}
+
+const thirdMusic = () => {
+    play_single_myFighter_sound()
+}
 
 const bossAttack = () => {
-    play_single_sound()
+    play_single_boss_sound()
+
     const lastBossIndex = bosses.length - 1;
 
     if (currentBossIndex > lastBossIndex) {
@@ -146,12 +204,7 @@ const bossAttack = () => {
 
     bossWrapper.innerHTML = createBoss(bosses[currentBossIndex])
 
-    addEventListener('click', ev => {
-        myFighter.innerHTML = ''
-        console.log('second')
-        // myFighter.img.src ="img/myFighter-second-position.svg"
-    })
-
+    setBossesInfo()
 }
 
 const startGame = () => {
@@ -160,6 +213,14 @@ const startGame = () => {
 
 startGame()
 
+const endGame = () => {
+    localStorage.clear()
+    fightWrapper.style.display = 'none';
+    formWrapper.style.display = 'block';
+    startGame()
+}
+
+exit.addEventListener('click', endGame)
 
 
 
